1d7be02297997a8d44e6726dbbd6e2e2218ba559
Zeus <97366514+AchievedZeus574@users.noreply.github.com>
2025-05-15 12:17:29 -0500

    Updated text styling to not use deprecated functions

--- src/main/java/com/github/K4RUNIO/simpleTimber/SimpleTimberCommand.java
+++ src/main/java/com/github/K4RUNIO/simpleTimber/SimpleTimberCommand.java
@@ -1,11 +1,13 @@
 package com.github.K4RUNIO.simpleTimber;
 
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 
 public class SimpleTimberCommand implements CommandExecutor {
 
@@ -19,15 +21,15 @@ public class SimpleTimberCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
             if (sender instanceof Player && !sender.hasPermission("simpletimber.reload")) {
-                sender.sendMessage(ChatColor.RED + "You don't have permission!");
+                sender.sendMessage(Component.text("You don't have permission!", NamedTextColor.RED));
                 return true;
             }
 
             plugin.getConfigManager().loadConfig();
-            sender.sendMessage(ChatColor.GREEN + "SimpleTimber config reloaded!");
+            sender.sendMessage(Component.text("SimpleTimber config reloaded!", NamedTextColor.GREEN));
             return true;
         }
-        sender.sendMessage(ChatColor.RED + "§l/simpletimber reload");
+        sender.sendMessage(Component.text("/simpletimber reload").color(NamedTextColor.RED).decorate(TextDecoration.BOLD));
         return true;
     }
 }
